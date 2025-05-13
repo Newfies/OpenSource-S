ff53d480a9cb6cef2c3559d63cd0e38990bc6141
Newfie <157941859+Newfies@users.noreply.github.com>
2025-05-13 10:04:21 -0500

    nop

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
     const c1 = document.getElementById('c1');
 
+    /*
     if (c1){
         c1.addEventListener('click', function(){
             c1.select();
@@ -9,5 +10,6 @@ document.addEventListener("DOMContentLoaded", function(){
     } else {
         console.log("No c1 found");
     }
+    */
 
 });
